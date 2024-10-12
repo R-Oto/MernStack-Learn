0a2739f32d47a102d4d1d35251e8a692feb06911
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-12 17:53:42 +0400

    setup tailwind

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const connect = require("./connect")
 const express = require('express')
 const cors = require('cors')
-const postRoutes = require('./postRoutes')
+const posts = require('./postRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
-app.use("/api/posts", postRoutes)
+app.use("/api/posts", posts)
 
 app.listen(PORT, () => {
     connect.connectToServer()
