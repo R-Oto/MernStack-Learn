0ca3061b741bad9c21fa8c55ce53e867adcbabbc
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-12 17:48:18 +0400

    postRoutes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const connect = require("./connect")
 const express = require('express')
 const cors = require('cors')
+const postRoutes = require('./postRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
+app.use("/api/posts", postRoutes)
 
 app.listen(PORT, () => {
+    connect.connectToServer()
     console.log("Server started")
 })
