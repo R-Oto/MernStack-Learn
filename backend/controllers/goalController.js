47dbf2c01e098a2c6400005e9ca65bcfd5991626
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-15 00:03:42 +0400

    backend completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,4 +1,5 @@
 const Goal = require("../models/goalModel")
+const User = require('../models/userModel')
 
 const getGoal = async (req,res) => {
     const goals = await Goal.find({user: req.user.id})
@@ -7,7 +8,8 @@ const getGoal = async (req,res) => {
 
 const createGoal = async (req,res) => {
     const goal = await Goal.create({
-        text: req.body.text
+        text: req.body.text,
+        user: req.user.id
     })
     res.status(201).json({message: "Goal created", goal})
 }
@@ -17,6 +19,15 @@ const updateGoal = async (req,res) => {
 
     if(!goal){
         return res.status(400).json({error: error.message})
+    }
+
+    const user = await User.findById(req.user.id)
+    if(!user){
+        res.status(401).json({error: "User not found"})
+    }
+
+    if(goal.user.toString() !== user.id){
+        res.status(401)
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
