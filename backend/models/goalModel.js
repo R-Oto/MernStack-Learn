d1db40fd1fc27319d6224fbc4a75da3d904985a6
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-14 11:14:41 +0400

    user model completed

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const goalSchema = new mongoose.Schema({
     user: {
-        
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
     },
     text: {
         type: String,
