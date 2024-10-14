e683f03c4b931dcd369c38396c59e0e80e12d285
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-14 22:12:50 +0400

    user controller setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,9 @@
+const User = require('../models/userModel')
+
+const registerUser = async (req,res)=>{
+    
+}
+
+module.exports ={
+    registerUser
+}
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 require('dotenv').config()
 const goalRouter = require('./routes/goalRoutes')
 const connectDB = require('./config/db')
+const userRouter = require('./routes/userRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json())
 app.use("/api/goals", goalRouter)
+app.use("/api/users", userRouter)
 
 app.listen(PORT, () => {
     connectDB()
