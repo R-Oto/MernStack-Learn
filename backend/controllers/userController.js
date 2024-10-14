3138457ec41537177450e6306b9c226bd75dc181
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-14 22:18:27 +0400

    controller setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,16 @@ const registerUser = async (req,res)=>{
     
 }
 
+const loginUser = async (req,res)=>{
+    
+}
+
+const getMe = async (req,res)=>{
+    
+}
+
 module.exports ={
-    registerUser
+    registerUser,
+    loginUser,
+    getMe
 }
