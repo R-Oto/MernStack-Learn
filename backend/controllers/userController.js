1eedd75d328dfba062a04aa8e8f6dcae7006a8f5
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-14 22:26:28 +0400

    hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,32 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler');
 
-const registerUser = async (req,res)=>{
-    
-}
+const registerUser = asyncHandler(async (req,res)=>{
+    const {name,email,password} = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({error: "Please fill in all fields"})
+    }
 
-const loginUser = async (req,res)=>{
-    
-}
+    const userExists = await User.findOne({email})
+    if(userExists){
+        return res.status(400).json({error: "User already exists"})
+    }
 
-const getMe = async (req,res)=>{
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const user = await User.create({name, email, password})
+})
+
+const loginUser = asyncHandler(async (req,res)=>{
     
-}
+})
+
+const getMe = asyncHandler(async (req,res)=>{
+    
+})
 
 module.exports ={
     registerUser,
