6a9ab217dbe1c4412461a2437866d2d5966bae5e
R.Oto <r.obusinesscommercial@gmail.com>
2024-10-16 00:00:07 +0400

    working with post

--- backend/controller/workoutController.js
+++ backend/controller/workoutController.js
@@ -2,11 +2,22 @@ const Workout = require('../model/workoutModel')
 const asyncHandler = require('express-async-handler')
 
 const getAll = asyncHandler(async (req,res) => {
-
+    
 })
 
 const createOne = asyncHandler(async (req,res) => {
-    
+    const {title, load, reps} = req.body;
+    if(!title || !load || !reps){
+        res.status(400).json({error: "All fields are required"})
+    }
+
+    try{
+        const workout = await Workout.create({title, load, reps})
+        
+        return res.status(201).json({message: "Workout created successfully", workout})
+    }catch(error){
+        return res.status(500).json({error: "Cant create workout"})
+    }
 })
 
 const getOne = asyncHandler(async (req,res) => {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const workoutRoute = require('./route/workoutRoute')
 
 require('dotenv').config()
 
@@ -11,6 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.json())
+app.use("/api/workout", workoutRoute)
 
 
 mongoose.connect(process.env.ATLAS_URI).then(()=>{
